eb6456a346ae9261699c595c70d4e84b3af21e3d
Martin Schneider <craftoid@k2g2.org>
2016-04-24 21:10:10 +0200

    check ace first

--- p5js-help.js
+++ p5js-help.js
@@ -1,7 +1,7 @@
 javascript: 
 (function() {		
 	var q;
-	q = "" + ( (window.getSelection&&window.getSelection()) || (document.getSelection&&document.getSelection()) || (document.selection&&document.selection.createRange&&document.selection.createRange().text) || (window.ace&&ace.getSession&&ace.getSession().doc.getTextRange(ace.selection.getRange())) );
+	q = "" +  ((window.ace&&ace.getSession&&ace.getSession().doc.getTextRange(ace.selection.getRange())) ||  (window.getSelection&&window.getSelection()) || (document.getSelection&&document.getSelection()) || (document.selection&&document.selection.createRange&&document.selection.createRange().text))
 	if(q == "") {
 		q = prompt("Search P5 Reference", "");
 	}
