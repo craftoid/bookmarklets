58820a16e0cdd990e92ac8657a1e7a98c94576b4
Martin Schneider <craftoid@k2g2.org>
2016-04-24 20:59:11 +0200

    Add support for text selection in the ACE editor

--- p5js-help.js
+++ p5js-help.js
@@ -1,7 +1,7 @@
 javascript: 
 (function() {		
 	var q;
-	q = "" + ((window.getSelection&&window.getSelection()) || (document.getSelection&&document.getSelection()) || (document.selection&&document.selection.createRange&&document.selection.createRange().text));
+	q = "" + ( (window.getSelection&&window.getSelection()) || (document.getSelection&&document.getSelection()) || (document.selection&&document.selection.createRange&&document.selection.createRange().text) || (window.ace&&ace.getSession&&ace.getSession().doc.getTextRange(ace.selection.getRange())) );
 	if(q == "") {
 		q = prompt("Search P5 Reference", "");
 	}
@@ -11,3 +11,5 @@ javascript:
 		location="http://p5js.org/reference/";
 	}
 })();
+
+
